de0f233b7e634896ae6f5579672cc0812b533f55
loganitis899 <league@WTS13.attlocal.net>
2018-03-12 20:00:49 -0700

    jeff
    did some algorithm stuff lol

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -4,8 +4,25 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0; //<- this needs changing
+		for (int i = 0; i < eggs.size(); i++) {
+			if(eggs.get(i).equals("cracked")) {
+				System.out.println(i);
+				return i;
+			}
+		}
+		return 0;
+	}
+	public static int countPearls(List<Boolean> oysters) {
+		int pearls = 0;
+		for (Boolean o : oysters) {
+			if(o) {
+				pearls++;
+			}
+		}
+		System.out.println(pearls);
+		return pearls;
 	}
 	
 	//Add other methods here
+	
 }

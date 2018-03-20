3a55cc88f82d7193fc4acfcae3569ecc2ba64e48
piyu1234 <league@WorthyMini-5.attlocal.net>
2018-03-19 20:01:12 -0700

    working on the test score sorter thing that is literally hell.

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -1,12 +1,15 @@
 package SearchingAndSortingAlgorithms;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
 		for (int i = 0; i < eggs.size(); i++) {
 			if(eggs.get(i).equals("cracked")) {
-				System.out.println(i);
+				
 				return i;
 			}
 		}
@@ -19,10 +22,63 @@ public class Algorithms {
 				pearls++;
 			}
 		}
-		System.out.println(pearls);
+		
 		return pearls;
 	}
+	public static double findTallest(List<Double> peeps) {
+		double tallest=0;
+		for (int i = 0; i < peeps.size(); i++) {
+			if(peeps.get(i)>=tallest) {
+			
+				tallest=peeps.get(i);
+			}
+		}
+		return tallest;
+	}
 	
+	public static String findLongestWord(List<String> words) {
+		String word="";
+		String longestWord="";
+		for (int i = 0; i < words.size(); i++) {
+			if(words.get(i).length()>longestWord.length()) {
+			
+				longestWord=words.get(i);
+				
+			}
+		}
+		return longestWord;
+	}
+	
+	public static boolean containsSOS(List<String> message) {
+			if (message.contains(" ... --- ... ")) {
+				
+				return true;
+			}
+		
+		return false;
+	}
+	
+	public static List<Double> sortScores(List<Double> scores){
+		List<Double>scoreList=new ArrayList<Double>();
+		scoreList.add(scores.get(0));
+		System.out.println(scores);
+		for (int i = 0; i < scores.size(); i++) {
+			
+			for (int j = 0; j < scoreList.size(); j++) {
+				if(scores.get(i)<scoreList.get(j)) {
+					scoreList.add((j-1), scores.get(i));
+				}
+				
+				else {
+					scoreList.add(scores.get(i));
+					
+				}
+			}
+			
+		}
+		System.out.println(scoreList);
+		return scoreList;
+	}
 	//Add other methods here
 	
 }
